f6d525429008e0fab4d5439f3302f349e247c702
mouni186 <mounikas1861999@gmail.com>
2022-05-16 22:17:39 +0530

    Feat: Card created in React using Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section>   
+    <div class="container mx-auto p-10 mt-5">
+    <div class="flex">                  
+            <div class=" mt-4 w-full lg:w-1/6 p-10 my-auto rounded-3xl bg-white shadow-lg">
+                    <div class="flex mt-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 bg-black text-2xl text-white p-1 rounded" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                                  </svg>
+                                  <h2 class="ml-5 text-xl font-semibold text-slate-800">Dashboard</h2>
+                    </div>
+                    <div class="flex mt-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                                  </svg>
+                                  <h2 class="ml-5 text-thin text-slate-200">Email</h2>
+                    </div>
+                    <div class="flex mt-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                  </svg>
+                                  <h2 class="ml-5 text-thin text-slate-200">Calendar</h2>
+                    </div>
+                    <div class="flex mt-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                                  </svg>
+                                  <h2 class="ml-5 text-thin text-slate-200">graph</h2>
+                    </div>
+                    <div class="flex mt-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                                  </svg>
+                                  <h2 class="ml-5 text-thin text-slate-200">Forms</h2>
+                    </div>
+                    
+            </div>
+            </div>
+            </div>
+            </section>
+
+  );
+}
+
+export default App;
